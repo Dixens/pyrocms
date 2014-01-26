7672041e9aa0bc7ca69b9aeaea30ce8a7404337c
Dixens <nicolasdupuy@dixens.net>
2014-01-26 16:25:17 +0100

    French translation updates

--- system/cms/modules/files/language/french/help_lang.php
+++ system/cms/modules/files/language/french/help_lang.php
@@ -4,21 +4,22 @@
 
 // inline help html. Only 'help_body' is used.
 $lang['help_body'] = "
-<h4>Overview</h4><hr>
-<p>The files module is an excellent way for the site admin to manage the files in use on the site.
-All images or files that are inserted into pages, galleries, or blog posts are stored here.
-For page content images you may upload them directly from the WYSIWYG editor or you can upload them here and just insert them via WYSIWYG.</p>
+<h4>Présentation</h4><hr>
+<p>Le module de fichiers permet à l\'administrateur du site de gérer les fichiers utilisés sur son site.
+Toutes les images ou les fichiers intégrés dans les pages, galeries ou les billets de blog sont stockés ici.
+En ce qui concerne plus particulièrement les images utilisées dans vos contenus de pages, vous pouvez les charger directement à partir de l\'éditeur WYSIWYG ou, alternativement, les charger dans le module de fichiers pour n\'avoir plus qu\'à les insérer ensuite via l'\éditeur WYSIWYG.</p>
 
-<h6>Managing Folders</h6>
-<p>After you create the top level folder or folders you may create as many subfolders as you need such as blog/images/screenshots/image.jpg and pages/audio/sample.mp3.
-The folder names are for your use only, the name is not displayed in the download link on the front end.
-To edit a folder select Manage Folders in the left column, then click Edit on the folder you wish to change.</p>
+<h6>Gestion des dossiers</h6>
+<p>Après avoir créé le ou le(s) dossier(s) de premier niveau, vous pouvez créer autant de sous-dossiers que vous le souhaitez, comme, par exemple, blog/images/screenshots/image.jpg et pages/audio/sample.mp3.
+L\'intitulé des dossiers n\'est qu\'à votre intention : il n\'est pas affiché dans le lien de téléchargement proposé sur le site public.
+Pour éditer un dossier, sélectionnez Gérer les dossiers dans la colonne de gauche puis cliquer sur Editer dans le dossier souhaité.</p>
 
-<h6>Managing Files</h6>
-<p>To manage files select the folder name in the left column. If the file is in a subfolder just choose the parent.
-Once you are in the parent folder you can choose the subfolder and filter by file type with the dropdowns at the top of the screen.
-Now that you are in the proper folder you may upload by clicking the Upload button beside the Filter dropdown.
-Note there are two ways to view your files: List and Grid. List shows file details while grid will show a thumbnail of the image.</p>
+<h6>Gérer les fichiers</h6>
+<p>Pour gérer les fichiers, sélectionnez le nom du dossier dans la colonne de gauche. Si le fichier se trouve dans un sous-dossier, sélectionnez simplement le dossier parent.
+Une fois dans le dossier parent, vous pouvez choisir un sous-dossier et filtrer par type de fichier au moyen du menu déroulant en haut de la page.
+
+Une fois dans le bon dossier, vous pouvez charger des fichiers en cliquant sur le bouton correspondant, à côté du menu déroulant Filtres.
+Notez qu\il existe deux manières de consulter vos fichiers : Liste et Grille. La liste shows file details while grid will show a thumbnail of the image.</p>
 
 <h6>Uploading Files</h6>
 <p>After clicking the Upload button in the desired folder an upload window will appear.
